9169ccef208c95dc28188af144352b5611b8a598
Nagesh <nagesh.k@tecnolynx.com>
2021-04-20 22:24:38 +0530

    changed the button and its icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pwd = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&
     btnCopyPassword = document.querySelector('.btnCopyPassword'),
     btnGeneratePassword = document.querySelector('.btnGeneratePassword');
 
+    // Generating Password
     const generatePassword = () => {
         let password = "";
       let passwordLength = document.getElementById("passwordLength").value;
@@ -14,6 +15,7 @@ let pwd = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&
       return password;      
     };    
 
+    // Generating Number of Passwords
     btnGeneratePassword.addEventListener('click', () => {
       let numberPasswords = document.getElementById("numberPasswords").value;
       passwords.innerHTML = '';
@@ -25,6 +27,7 @@ let pwd = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&
       passwords.style.color = 'black';      
     });
 
+    // Copying to the Clipboard
     btnCopyPassword.addEventListener('click', () => {        
         let textarea = document.createElement('textarea');
         textarea.setAttribute('readonly', '');
@@ -34,4 +37,9 @@ let pwd = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&
         textarea.select();
         document.execCommand('copy');
         document.body.removeChild(textarea);
+        btnCopyPassword.innerHTML = '<i class="fas fa-copy"></i> Copied Password(s)';
+
+        setTimeout(()=>{
+            btnCopyPassword.innerHTML = '<i class="far fa-copy"></i> Copy Password(s)';
+        }, 3000)
     });
